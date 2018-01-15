f00134237671bc6e0a07bb8b0dbbcc1e5bb6caec
Guillermo Narvaja <guillermo.narvaja@radiocut.fm>
2018-01-15 03:10:19 -0300

    Initial version.

--- www/hello.js
+++ www/hello.js
@@ -4,4 +4,5 @@ module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
     }
+    // TODO: add moments SDK functions
 };
